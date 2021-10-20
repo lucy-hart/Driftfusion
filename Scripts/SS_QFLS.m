16e60fd94ffa295966e37b655fb03b0e6356843e
lucyhart.042 <lucyhart.042@gmail.com>
2021-10-20 14:01:50 +0100

    Fixing stuff

--- Scripts/SS_QFLS.m
+++ Scripts/SS_QFLS.m
@@ -20,17 +20,18 @@ par_90 = refresh_device(par_90);
 devices = {par_10, par_50, par_90};
 
 %% Find eqm solutions at 1 sun intensity
-eqm_solutions = cell(1,3);
+eqm_solutions_dark = cell(1,3);
+eqm_solutions_light = cell(1,3);
 for i = 1:3
-    eqm = equilibrate(devices{i});
-    eqm_solutions{i} = changeLight(eqm.el,1,1);
+    eqm_solutions_dark{i} = equilibrate(devices{i});
+    eqm_solutions_light{i} = changeLight(eqm.el,1,1);
 end
 
 %% Perform CV scans
 CV_solutions = cell(1,3);
 for j = 1:3
-    sol = eqm_solutions{j};
-    CV_solutions{j} = doCV(sol, 1, 0, 1.2, 0, 100e-3, 1, 241);
+    sol = eqm_solutions_dark{j};
+    CV_solutions{j} = doCV(sol.el, 1, 0, 1.2, 0, 100e-3, 1, 241);
     dfplot.JtotVapp(CV_solutions{j},0)
     hold on
 end
