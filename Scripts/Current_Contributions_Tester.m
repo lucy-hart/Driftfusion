86b9d2fd9d609b1c10f547e18e944d2d76524691
Lucy <lucy.hart18@imperial.ac.uk>
2021-11-23 17:42:39 +0000

    changing test file

--- Scripts/Current_Contributions_Tester.m
+++ Scripts/Current_Contributions_Tester.m
@@ -1,6 +1,6 @@
-par = pc('Input_files/3_layer_test.csv');
+par = pc('Input_files/1_layer_test.csv');
 eqm = equilibrate(par);
-CV_solution = doCV(eqm.ion, 1, 0, 1.0, 0, 10e-3, 1, 211);
+CV_solution = doCV(eqm.ion, 1, 0, 1.1, 0, 10e-3, 1, 211);
 dfplot.JtotVapp(CV_solution,0)
 stats = CVstats(CV_solution);
 Plot_Current_Contributions(CV_solution)
