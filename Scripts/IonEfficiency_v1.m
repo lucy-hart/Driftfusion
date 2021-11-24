ca7d08bc22f458d6acd0817c51d3ff1d8490cf93
Lucy <lucy.hart18@imperial.ac.uk>
2021-11-24 19:09:23 +0000

    Made parameter for loop

--- Scripts/IonEfficiency_v1.m
+++ Scripts/IonEfficiency_v1.m
@@ -1,19 +1,38 @@
-%Program to do a parameter sweep of E_bi and recombination velocity for a single
+%Program to do a parameter sweep of Ebi and recombination velocity for a single
 %layer MAPI cell and see how device parameters are affected by the presence
 %of ions.
+
+
+%% Define parameter space
+% Rows are recombination velocities, columns are Ebi values
+params = cell(7,12);
+sigma_values = logspace(1,7,7);
+Ebi_values = linspace(0,0.6,12);
+for i=1:7
+    for j=1:12
+        params{i,j} = [sigma_values(i), Ebi_values(j)];
+    end
+end
 
 %% Read in basic device
 par=pc('Input_files/1_layer_test.csv');
 
-%% Define parameter space
-% Rows are recombination velocities, columns are E_bi values
-params = zeros(7,12);
-sigma_values = logspace(1,7,7);
-E_BI_values = linspace(0,1.2,12);
+%% Do (many) JV sweeps
+ion_results = cell(7,12);
+el_results = cell(7,12);
 for i=1:7
     for j=1:12
-        params(i,j) = [sigma_values()]
-
-%% Do (many) JV sweeps
+        par.Phi_left = par.EF0-params{i,j}(2);
+        par.Phi_right = par.EF0+params{i,j}(2);
+        par.sn_l = params{i,j}(2);
+        par.sp_r = params{i,j}(2);
+        par = refresh_device(par);
+        soleq = equilibrate(par);
+        solCV_ion = doCV(soleq.ion, 1, -0.1, 2*params{i,j}(2) + 0.4, -0.1, 10e-3, 1, 241);
+        solCV_el = doCV(soleq.el, 1, -0.1, 2*params{i,j}(2) + 0.4, -0.1, 10e-3, 1, 241);
+        ion_results{i,j} = CVstats(solCV_ion);
+        el_results{i,j} = CVstats(solCV_el);
+    end
+end
 
 %% Plot results
