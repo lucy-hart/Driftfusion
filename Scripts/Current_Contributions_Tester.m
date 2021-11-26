3d98e8ebcf83c9078676f473e7ae74cff2f66c35
pdb13 <p.calado13@imperial.ac.uk>
2021-11-26 15:55:02 +0000

    remove JtotVapp plot

--- Scripts/Current_Contributions_Tester.m
+++ Scripts/Current_Contributions_Tester.m
@@ -1,9 +1,8 @@
-par = pc('Input_files/3_layer_test.csv');
+par = pc('Input_files/spiro_mapi_tio2.csv');
 eqm = equilibrate(par);
 CV_solution = doCV(eqm.ion, 1, 0, 1.1, 0, 10e-3, 1, 211);
 
 %% Plots
-dfplot.JtotVapp(CV_solution,0)
 Plot_Current_Contributions(CV_solution)
 ylim([-25e-3, 25e-3])
 stats = CVstats(CV_solution);
