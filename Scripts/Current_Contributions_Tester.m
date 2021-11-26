aa206b30a6293c633aa8d97f3b7d0310a9a44583
pdb13 <p.calado13@imperial.ac.uk>
2021-11-26 15:40:50 +0000

    Updated Plot_Current_Contributions to include surface recombination components at the system boundaries

--- Scripts/Current_Contributions_Tester.m
+++ Scripts/Current_Contributions_Tester.m
@@ -1,6 +1,9 @@
-par = pc('Input_files/1_layer_test.csv');
+par = pc('Input_files/3_layer_test.csv');
 eqm = equilibrate(par);
 CV_solution = doCV(eqm.ion, 1, 0, 1.1, 0, 10e-3, 1, 211);
+
+%% Plots
 dfplot.JtotVapp(CV_solution,0)
-stats = CVstats(CV_solution);
 Plot_Current_Contributions(CV_solution)
+ylim([-25e-3, 25e-3])
+stats = CVstats(CV_solution);
