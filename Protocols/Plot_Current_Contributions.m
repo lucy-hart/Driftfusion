361919efd2de734b44c1cea518fdef0957301b76
lucyhart.042 <lucyhart.042@gmail.com>
2021-11-26 21:09:00 +0000

    Updating current contribution plots

--- Protocols/Plot_Current_Contributions.m
+++ Protocols/Plot_Current_Contributions.m
@@ -1,13 +1,13 @@
 function Plot_Current_Contributions(CVsol)
-%Script to plot the currents from radiative and non-radiative (split into SRH
-%and VSR) losses, alongside the current measured in a JV sweep and the
+%Script to plot the currents from radiative and non-radiative (split into
+%SRH, surface and VSR) losses, alongside the current measured in a JV sweep and the
 %generation current. 
 
 %% Break down contributions to the current
 %Columns in J_values are J_gen, J_rad, J_srh, J_vsr and J_ext
 num_values = length(CVsol.t);
 J_values = zeros(num_values, 6);
-e = CVsol.par.e;
+e = -CVsol.par.e;
 loss_currents = dfana.calcr(CVsol,'sub');
 x = CVsol.par.x_sub;
 gxt = dfana.calcg(CVsol);
@@ -15,7 +15,7 @@ J = dfana.calcJ(CVsol);
 j_surf_rec = dfana.calcj_surf_rec(CVsol);
 
 %forward sweep
-J_values(:,1) = -e*trapz(x, gxt(1,:))';
+J_values(:,1) = e*trapz(x, gxt(1,:))';
 J_values(:,2) = e*trapz(x, loss_currents.btb, 2)';
 J_values(:,3) = e*trapz(x, loss_currents.srh, 2)';
 J_values(:,4) = e*trapz(x, loss_currents.vsr, 2)';
@@ -24,7 +24,8 @@ J_values(:,6) = J.tot(:,1);
     
 %% Plot contributons to the current
 figure(2)
-line_colour = {[0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250], [0 0.4470 0.7410], [0.3010 0.7450 0.9330], [0.4660 0.6740 0.1880], [0.6740 0.4660 0.1880]};
+line_colour = {[0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250], [0.4940 0.1840 0.5560],...
+                [0 0.4470 0.7410], [0.3010 0.7450 0.9330], [0.4660 0.6740 0.1880]};
 V = dfana.calcVapp(CVsol);
 for n = 1:6
     plot(V(:), J_values(:,n), 'color', line_colour{n})
@@ -34,7 +35,7 @@ plot(V(1:num_values), zeros(1,num_values), 'black', 'LineWidth', 1)
 hold off
 xlim([0, CVsol.par.V_fun_arg(2)])
 xlabel('Voltage (V)')
-ylim([-0.025, 0.025])
+ylim([-0.025, 0.01])
 ylabel('Current Density (Acm^{-2})')
 legend({'J_{gen}', 'J_{rad}', 'J_{SRH}', 'J_{VSR}', 'J_{surf}', 'J_{ext}'}, 'Location', 'bestoutside')
 
--- Scripts/Weidong_ETL.m
+++ Scripts/Weidong_ETL.m
@@ -45,40 +45,9 @@ ylim([-0.03, 0.01])
 xlabel('Voltage(V)')
 ylabel('Current Density (Acm^{-2})')
 
-%% Break down contributions to the current
-%Columns in J_values are J_gen, J_rad, J_srh, J_vsr and J_ext
-J_values = zeros(321,5,3);
-e = 1.60e-19;
+%% Plot current contributions
 
-%choose to plot for electron vs ion solutions - see what difference this
-%makes
-CV_solutions = CV_solutions_ion;
-
-for k = 1:3
-    loss_currents = dfana.calcr(CV_solutions{k},'whole');
-    J_values(:,1,k) = -e*trapz(CV_solutions{k}.par.x_sub, dfana.calcg(CV_solutions{k}).').';
-    J_values(:,2,k) = -e*trapz(CV_solutions{k}.par.x_sub, loss_currents.btb(:,1:end-1).').';
-    J_values(:,3,k) = -e*trapz(CV_solutions{k}.par.x_sub, loss_currents.srh(:,1:end-1).').';
-    J_values(:,4,k) = -e*trapz(CV_solutions{k}.par.x_sub, loss_currents.vsr(:,1:end-1).').';
-    J_values(:,5,k) = dfana.calcJ(CV_solutions{k}).tot(:,1);
-    
-end
-
-%% Plot contributons to the current
-figure(2)
-line_colour = {[0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250], [0 0.4470 0.7410], [0.3010 0.7450 0.9330], [0.4660 0.6740 0.1880]};
-V = dfana.calcVapp(CV_solutions{1});
-hold on
-for n = 1:5
-    plot(V(:), J_values(:,n,3), 'color', line_colour{n}) 
-end
-plot(V(:), zeros(1,length(V)), 'black', 'LineWidth', 1)
-hold off
-xlim([0, 1.3])
-xlabel('Voltage (V)')
-ylim([-0.025, 0.025])
-ylabel('Current Density (Acm^{-2})')
-legend({'J_{gen}', 'J_{rad}', 'J_{SRH}', 'J_{VSR}', 'J_{ext}', ''}, 'Location', 'bestoutside')
+Plot_Current_Contributions(CV_solutions_el{2})
 
 %% Plot 'PL' results
 figure(3)
