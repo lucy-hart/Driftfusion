cb911875642d2d2d94230a9912e884f41dec7c16
lucyhart.042 <lucyhart.042@gmail.com>
2021-12-21 07:10:25 +0000

    20/12/21

--- Scripts/QuickJVChecker.m
+++ Scripts/QuickJVChecker.m
@@ -1,6 +1,6 @@
-%par = pc('Input_files/PTAA_MAPI_Kloc6_v2.csv');
+par = pc('Input_files/PTAA_MAPI_Kloc6_v2.csv');
 %par = pc('Input_files/PTAA_MAPI_PCBM_v2.csv');
-par = pc('Input_files/PTAA_MAPI_ICBA_v2.csv');
+%par = pc('Input_files/PTAA_MAPI_ICBA_v2.csv');
 
 eqm = equilibrate(par);
 CV_sol_ion = doCV(eqm.ion, 1.15, -0.3, 1.3, -0.3, 1e-3, 1, 341);
@@ -9,16 +9,24 @@ Plot_Current_Contributions(CV_sol_ion)
 legend('data', 'Location', 'northwest')
 CVstats(CV_sol_ion)
 
-%% Add shunt resistance 
+%% Try and add a shunt resistance
 
-J_tot = dfana.calcJ(CV_sol_ion).tot;
-V = dfana.calcVapp(CV_sol_ion)';
-R_shunt = 20;
-J_ext = J_tot(:,1) + V/R_shunt;
-figure(7)
-plot(V, J_ext)
+Area = 0.045e-4;
+R_shunt = 3e6;
+J_SS = dfana.calcJ(CV_sol_ion).tot;
+Vapp = dfana.calcVapp(CV_sol_ion)';
+R_SS = Vapp./(J_SS*Area);
+R_T = (R_shunt*R_SS)./(R_shunt+R_SS);
+J_out = Vapp./(R_T*Area);
+figure(666)
+plot(Vapp, J_SS)
 hold on
-plot(V, J_tot(:,1))
+plot(Vapp, J_out)
 hold off
+legend('Driftfusion', 'With shunt?')
+
+
+
+
 
      
