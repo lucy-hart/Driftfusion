5c5becd7c35c961cb0ab5f8ab508497ea71b1033
pdb13 <p.calado13@imperial.ac.uk>
2022-01-06 10:48:13 +0000

    Added try loops and error_log

--- Scripts/IonEfficiency_v1.m
+++ Scripts/IonEfficiency_v1.m
@@ -4,80 +4,109 @@
 tic
 %% Define parameter space
 % Rows are recombination velocities, columns are Ebi values
-params = cell(7,14);
-sigma_values = logspace(-1,5,7);
-Ebi_values = zeros(1,14);
-Ebi_values(2:end) = linspace(0,0.6,13);
-Ebi_values(2) = 0.01;
+sigma_values = logspace(1, 4, 5);
+Ebi_values = linspace(0, 0.5, 6);
+Nsigma = length(sigma_values); 
+NEbi = length(Ebi_values);
+params = cell(Nsigma, NEbi);
 
-for i=1:7
-    for j=1:14
+for i=1:Nsigma
+    for j=1:NEbi
             params{i,j} = [sigma_values(i), Ebi_values(j)];
     end
 end
 
-ion_results = cell(7,14);
-el_results = cell(7,14);
+error_log = zeros(Nsigma, NEbi);
+ion_results = cell(Nsigma, NEbi);
+el_results = cell(Nsigma, NEbi);
 %% Do (many) JV sweeps
 par=pc('Input_files/1_layer_test.csv');
-for i=1:7
-    for j=1:14
-        if j==1 && i ==7
-            par.Phi_left = par.EF0-0.01;
-            par.Phi_right = par.EF0+0.01;
-        else
-            par.Phi_left = par.EF0-params{i,j}(2);
-            par.Phi_right = par.EF0+params{i,j}(2);  
-        end
+for i=1:Nsigma
+    for j=1:NEbi
+        disp(["minority carrier SRV = ", num2str(sigma_values(i)), "cm s-1"])
+        disp(["Built in potential, Vbi = ", num2str(Ebi_values(j)), " V"])
+%         if j== 1 && i == Nsigma
+%             par.Phi_left = par.EF0 - 0.01;
+%             par.Phi_right = par.EF0 + 0.01;
+%         else
+            par.Phi_left = par.EF0 - params{i,j}(2);
+            par.Phi_right = par.EF0 + params{i,j}(2);  
+%         end
         par.sn_l = params{i,j}(1);
         par.sp_r = params{i,j}(1);
         par = refresh_device(par);
-        soleq = equilibrate(par);
-        if params{i,j}(2) <= 0.35
-            solCV_ion = doCV(soleq.ion, 1, -0.25, 1.1, -0.25, 1e-4, 1, 241);
-            solCV_el = doCV(soleq.el, 1, -0.25, 1.1, -0.25, 1e-4, 1, 241);
-        elseif params{i,j}(2) > 0.35
-            solCV_ion = doCV(soleq.ion, 1, -0.25, 1.25, -0.25, 1e-4, 1, 241);
-            solCV_el = doCV(soleq.el, 1, -0.25, 1.25, -0.25, 1e-4, 1, 241);
+        
+        soleq{i, j} = equilibrate(par);
+        try
+            if params{i,j}(2) <= 0.35
+                solCV_ion{i, j} = doCV(soleq{i, j}.ion, 1, -0.1, 1, -0.1, 1e-4, 1, 241);
+                solCV_el{i, j} = doCV(soleq{i, j}.el, 1, -0.1, 1, -0.1, 1e-4, 1, 241);
+            elseif params{i,j}(2) > 0.35
+                solCV_ion{i, j} = doCV(soleq{i, j}.ion, 1, -0.1, 1.2, -0.1, 1e-4, 1, 241);
+                solCV_el{i, j} = doCV(soleq{i, j}.el, 1, -0.1, 1.2, -0.1, 1e-4, 1, 241);
+            end
+            error_log(i,j) = 0;
+            ion_results{i,j} = CVstats(solCV_ion{i, j});
+            el_results{i,j} = CVstats(solCV_el{i, j});
+        catch
+            warning("JV solution failed, try reducing Vmax")
+            error_log(i,j) = 1;
         end
-        ion_results{i,j} = CVstats(solCV_ion);
-        el_results{i,j} = CVstats(solCV_el);
     end
 end
 
 toc
 %% Calculate figure of merit
-PCE_ratio = zeros(7,14);
-for i=1:7
-    for j=1:14
+PCE_ratio = zeros(Nsigma, NEbi);
+for i=1:Nsigma
+    for j=1:NEbi
         try
             PCE_ratio(i,j) = el_results{i,j}.efficiency_f/ion_results{i,j}.efficiency_f;
+            PCE_el(i,j) = el_results{i,j}.efficiency_f;
+            PCE_ion(i,j) = ion_results{i,j}.efficiency_f;
         catch
             warning('CVstats unsucessful. Assigning a PCE value of 0.');
-            PCE_ratio(i,j) = 0;
+            PCE_el(i,j) = NaN;
+            PCE_ion(i,j) = NaN;
+            PCE_ratio(i,j) = NaN;
         end
     end
 end
 
 %% Plot results 
-PCE_ratio(6:7,1) = 0;
 figure(1)
-contourf(2*Ebi_values, log10(sigma_values), log10(PCE_ratio), 12, 'LineWidth', 0.1)
+contourf(2*Ebi_values, log10(sigma_values), PCE_ratio, 12, 'LineWidth', 0.1)
 xlabel('V_{BI} (V)')
 ylabel('log_{10}(s_{surf} /cms^{-1})')
 c = colorbar;
-c.Label.String = 'log_{10}(PCE_{el} / PCE_{ion})';
+c.Label.String = 'PCE_{el} / PCE_{ion}';
+
+%% Plot results 
+figure(12)
+contourf(2*Ebi_values, log10(sigma_values), PCE_ion, 12, 'LineWidth', 0.1)
+xlabel('V_{BI} (V)')
+ylabel('log_{10}(s_{surf} /cms^{-1})')
+c = colorbar;
+c.Label.String = 'PCE_{ion}';
+
+%% Plot results 
+figure(13)
+contourf(2*Ebi_values, log10(sigma_values), PCE_el, 12, 'LineWidth', 0.1)
+xlabel('V_{BI} (V)')
+ylabel('log_{10}(s_{surf} /cms^{-1})')
+c = colorbar;
+c.Label.String = 'PCE_{el}';
 
 %% Voc vs V_bi plot
 
-V_ratio = zeros(7,13);
-for i = 1:7
-    for j = 1:13
-        V_ratio(i,j) = ion_results{i,j+1}.Voc_f/(2*Ebi_values(j+1));
+V_ratio = zeros(Nsigma,NEbi);
+for i = 1:Nsigma
+    for j = 1:NEbi
+        V_ratio(i,j) = ion_results{i,j}.Voc_f/(2*Ebi_values(j));
     end
 end
 figure(2)
-contourf(2*Ebi_values(2:end), log10(sigma_values), log10(V_ratio), 12, 'LineWidth', 1)
+contourf(2*Ebi_values(1:end), log10(sigma_values), log10(V_ratio), 12, 'LineWidth', 1)
 xlabel('V_{BI} (V)')
 ylabel('log_{10}(s_{surf} /cms^{-1})')
 c = colorbar;
