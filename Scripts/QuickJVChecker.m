7f32ad50605c117001573dd4ff9a64bf7be47e15
lucyhart.042 <lucyhart.042@gmail.com>
2022-01-13 18:08:06 +0000

    13/01/21

--- Scripts/QuickJVChecker.m
+++ Scripts/QuickJVChecker.m
@@ -1,6 +1,6 @@
-par = pc('Input_files/PTAA_MAPI_Kloc6_v2.csv');
+%par = pc('Input_files/PTAA_MAPI_Kloc6_v2.csv');
 %par = pc('Input_files/PTAA_MAPI_PCBM_v2.csv');
-%par = pc('Input_files/PTAA_MAPI_ICBA_v2.csv');
+par = pc('Input_files/PTAA_MAPI_ICBA_v2.csv');
 
 eqm = equilibrate(par);
 CV_sol_ion = doCV(eqm.ion, 1.2, -0.2, 1.2, -0.2, 1e-3, 1, 281);
