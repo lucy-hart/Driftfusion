38f2435ea8790e36530e4fe5219eb6d3777d0896
Lucy <lucy.hart18@imperial.ac.uk>
2022-02-17 17:00:11 +0000

    17/02/22

--- Scripts/IonEfficiency_v3_3LayerSymmetricDevice.m
+++ Scripts/IonEfficiency_v3_3LayerSymmetricDevice.m
@@ -4,34 +4,39 @@ tic
 
 IonConc_values = [1e15, 1e16, 1e17, 1e18, 1e19];
 %Use these vaues for surface recombination 
-sigma_values= logspace(-1,5,7);
-Nsigma = length(sigma_values); 
-NIonConc = length(IonConc_values);
-params = cell(Nsigma, NIonConc);
+%y_values= logspace(-1,5,7);
+%Use these vaues for tau_SRH
+y_values = logspace(-9,-4,6);
 
-for i=1:Nsigma
+N_y = length(y_values); 
+NIonConc = length(IonConc_values);
+params = cell(N_y, NIonConc);
+
+for i=1:N_y
     for j=1:NIonConc
-            params{i,j} = [sigma_values(i), IonConc_values(j)];
+            params{i,j} = [y_values(i), IonConc_values(j)];
     end
 end
 
-error_log = zeros(Nsigma, NIonConc);
-soleq = cell(Nsigma, NIonConc);
-solCV_ion = cell(Nsigma, NIonConc);
-solCV_el = cell(Nsigma, NIonConc);
-ion_results = cell(Nsigma, NIonConc);
-el_results = cell(Nsigma, NIonConc);
+error_log = zeros(N_y, NIonConc);
+soleq = cell(N_y, NIonConc);
+solCV_ion = cell(N_y, NIonConc);
+solCV_el = cell(N_y, NIonConc);
+ion_results = cell(N_y, NIonConc);
+el_results = cell(N_y, NIonConc);
 
 %% Do (many) JV sweeps
 par=pc('Input_files/3_layer_test_symmetric.csv');
-for i=1:Nsigma
+for i=1:N_y
     for j=1:NIonConc
-        disp(["minority carrier SRV = ", num2str(sigma_values(i)), "cm s-1"])
+        disp(["tau_SRH = ", num2str(y_values(i)), " s"])
+        %disp(["minority carrier SRV = ", num2str(y_values(i)), "cm s-1"])
         disp(["Ion conc, N_cat = ", num2str(IonConc_values(j)), " cm-3"])
         par.Ncat(:) = params{i,j}(2);
         par.Nani(:) = params{i,j}(2);
-        par.sn(2) = params{i,j}(1);
-        par.sp(4) = params{i,j}(1);
+        %Remember to change these when you change y variable
+        par.taun(3) = params{i,j}(1);
+        par.taup(3) = params{i,j}(1);
         par = refresh_device(par);
 
         soleq{i,j} = equilibrate(par);
@@ -50,8 +55,8 @@ end
 
 toc
 %% Calculate figure of merit
-PCE_ratio = zeros(Nsigma, NIonConc);
-for i=1:Nsigma
+PCE_ratio = zeros(N_y, NIonConc);
+for i=1:N_y
     for j=1:NIonConc
         try
             PCE_ratio(i,j) = el_results{i,j}.efficiency_f/ion_results{i,j}.efficiency_f;
@@ -64,8 +69,14 @@ end
 
 %% Plot results 
 figure(1)
-contourf(log10(IonConc_values), log10(sigma_values(1:end)), log10(PCE_ratio(1:end,:)), 25, 'LineWidth', 0.1)
+contourf(log10(IonConc_values), log10(y_values(1:end)), log10(PCE_ratio(1:end,:)), 25, 'LineWidth', 0.1)
 xlabel('log_{10}(Mobile Cation Concentration /cm^{-3})')
-ylabel('log_{10}(s_{surf} /cms^{-1})')
+%ylabel('log_{10}(s_{surf} /cms^{-1})')
+ylabel('log_{10}(\tau_{SRH} /s)')
 c = colorbar;
 c.Label.String = 'log_{10}(PCE_{el} / PCE_{ion})';
+
+%% Save results and solutions
+
+filename = 'tld_symmetric_tSRH_vs_Ncat.mat';
+save(filename, 'el_results', 'ion_results', 'solCV_el', 'solCV_ion')
--- Scripts/weird_cases.m
+++ Scripts/weird_cases.m
@@ -1,17 +1,22 @@
-V_bi = 1;
-surf_vel = 1;
+Ncat = 1;
+y_var = 1;
 
-J_ion = dfana.calcJ(solCV_ion{surf_vel, V_bi});
-J_el = dfana.calcJ(solCV_el{surf_vel, V_bi});
-Vapp = dfana.calcVapp(solCV_ion{surf_vel, V_bi});
+J_ion = dfana.calcJ(solCV_ion{y_var, Ncat});
+J_el = dfana.calcJ(solCV_el{y_var, Ncat});
+Vapp = dfana.calcVapp(solCV_ion{y_var, Ncat});
 
 figure(008)
 plot(Vapp, J_el.tot(:,1), 'blue', Vapp, J_ion.tot(:,1), 'red')
 
 %% SS solutions under illumination
 
-el_light = changeLight(soleq{surf_vel, V_bi}.el, 1, 0);
-ion_light = changeLight(soleq{surf_vel, V_bi}.ion, 1, 0);
+el_light = changeLight(soleq{y_var, V_bi}.el, 1, 0);
+ion_light = changeLight(soleq{y_var, V_bi}.ion, 1, 0);
 
+%%
+Ncat = 3;
+y_var = 6;
 
+solCV_ion_single = doCV(soleq{y_var, Ncat}.ion, 1, -0.1, 1.2, -0.1, 1e-4, 1, 241);
+solCV_el_single = doCV(soleq{y_var, Ncat}.el, 1, -0.1, 1.2, -0.1, 1e-4, 1, 241);
 
