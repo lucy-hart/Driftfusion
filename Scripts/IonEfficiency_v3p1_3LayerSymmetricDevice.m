a00a1674497f7d90f80f4286d410a2b23cc20e38
Lucy <lucy.hart18@imperial.ac.uk>
2022-02-24 09:29:55 +0000

    23/02/22

--- Scripts/IonEfficiency_v3p1_3LayerSymmetricDevice.m
+++ Scripts/IonEfficiency_v3p1_3LayerSymmetricDevice.m
@@ -1,14 +1,15 @@
-%Use this file for non-energy related variables (tau_SRH and s_min)
+%Use this file for energy related things (TL Fermi levels, energy offsets
+%and Vbi)
 
 tic
 %% Define parameter space
 %Rows are variable parameter, columns are the ion concentrations
 
 IonConc_values = [1e15, 1e16, 1e17, 1e18, 1e19];
-%Use these vaues for surface recombination 
-y_values= logspace(-1,5,7);
-%Use these vaues for tau_SRH
-%y_values = logspace(-9,-4,6);
+%Use these vaues for Vbi 
+y_values= zeros(1,14);
+y_values(2:end) = linspace(0,0.6,13);
+y_values(2) = 0.01;
 
 N_y = length(y_values); 
 NIonConc = length(IonConc_values);
@@ -32,22 +33,24 @@ par=pc('Input_files/3_layer_test_symmetric.csv');
 %For SRH reset tau values in transport layers too
 for i=1:N_y
     for j=1:NIonConc
-        %disp(["tau_SRH = ", num2str(y_values(i)), " s"])
-        disp(["minority carrier SRV = ", num2str(y_values(i)), "cm s-1"])
-        disp(["Ion conc, N_cat = ", num2str(IonConc_values(j)), " cm-3"])
+        disp(["Ion Concentration = ", num2str(IonConc_values(i)), " cm-3"])
+        disp(["Built in potential, Vbi = ", num2str(2*y_values(j)), " V"])
         par.Ncat(:) = params{i,j}(2);
         par.Nani(:) = params{i,j}(2);
         %Remember to change these when you change y variable
-        par.sn(2) = params{i,j}(1);
-        par.sp(4) = params{i,j}(1);
-        %par.taun([1,3,5]) = params{i,j}(1);
-        %par.taup([1,3,5]) = params{i,j}(1);
+        par.Phi_left = par.EF0(3) - params{i,j}(1);
+        par.Phi_right = par.EF0(3) + params{i,j}(1);
         par = refresh_device(par);
 
         soleq{i,j} = equilibrate(par);
         try
-            solCV_ion{i, j} = doCV(soleq{i, j}.ion, 1, -0.1, 1.4, -0.1, 1e-4, 1, 241);
-            solCV_el{i, j} = doCV(soleq{i, j}.el, 1, -0.1, 1.4, -0.1, 1e-4, 1, 241);
+            if params{i,j}(1) <= 0.35
+                solCV_ion{i, j} = doCV(soleq{i, j}.ion, 1, -0.1, 1, -0.1, 1e-4, 1, 241);
+                solCV_el{i, j} = doCV(soleq{i, j}.el, 1, -0.1, 1, -0.1, 1e-4, 1, 241);
+            elseif params{i,j}(1) > 0.35
+                solCV_ion{i, j} = doCV(soleq{i, j}.ion, 1, -0.2, 1.3, -0.2, 1e-4, 1, 241);
+                solCV_el{i, j} = doCV(soleq{i, j}.el, 1, -0.2, 1.3, -0.2, 1e-4, 1, 241);
+            end
             error_log(i,j) = 0;
             ion_results{i,j} = CVstats(solCV_ion{i, j});
             el_results{i,j} = CVstats(solCV_el{i, j});
@@ -74,14 +77,15 @@ end
 
 %% Plot results 
 figure(1)
-contourf(log10(IonConc_values), log10(y_values(1:end)), log10(PCE_ratio(1:end,:)), 25, 'LineWidth', 0.1)
+contourf(log10(IonConc_values), 2*y_values, log10(PCE_ratio(1:end,:)), 25, 'LineWidth', 0.1)
 xlabel('log_{10}(Mobile Cation Concentration /cm^{-3})')
-ylabel('log_{10}(s_{min} /cms^{-1})')
-%ylabel('log_{10}(\tau_{SRH} /s)')
+%ylabel('log_{10}(s_{surf} /cms^{-1})')
+ylabel('log_{10}(V_{BI} /V)')
+ylim([-7,-4])
 c = colorbar;
 c.Label.String = 'log_{10}(PCE_{el} / PCE_{ion})';
 
 %% Save results and solutions
 
-filename = 'tld_symmetric_smin_vs_Ncat.mat';
+filename = 'tld_symmetric_Vbi_vs_Ncat_DopedTLs.mat';
 save(filename, 'el_results', 'ion_results', 'solCV_el', 'solCV_ion')
--- Scripts/weird_cases.m
+++ Scripts/weird_cases.m
@@ -8,15 +8,10 @@ Vapp = dfana.calcVapp(solCV_ion{y_var, Ncat});
 figure(008)
 plot(Vapp, J_el.tot(:,1), 'blue', Vapp, J_ion.tot(:,1), 'red')
 
-%% SS solutions under illumination
-
-el_light = changeLight(soleq{y_var, V_bi}.el, 1, 0);
-ion_light = changeLight(soleq{y_var, V_bi}.ion, 1, 0);
-
 %%
-Ncat = 1;
-y_var = 1;
+Ncat = 5;
+y_var = 4;
 
-solCV_ion_single = doCV(soleq{y_var, Ncat}.ion, 1, -0.1, 1.89, -0.1, 1e-3, 1, 241);
-%solCV_el_single = doCV(soleq{y_var, Ncat}.el, 1, -0.1, 1.85, -0.1, 1e-4, 1, 241);
+solCV_ion_single = doCV(soleq{y_var, Ncat}.ion, 1, -0.1, 1.2, -0.1, 1e-3, 1, 241);
+solCV_el_single = doCV(soleq{y_var, Ncat}.el, 1, -0.1, 1.2, -0.1, 1e-4, 1, 241);
 
