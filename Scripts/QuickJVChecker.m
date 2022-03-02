8baf328483da45205443ede51bb5554000ea8c8a
Lucy <lucy.hart18@imperial.ac.uk>
2022-03-02 11:44:30 +0000

    Twiddles since I last updated

--- Scripts/QuickJVChecker.m
+++ Scripts/QuickJVChecker.m
@@ -1,6 +1,6 @@
-par = pc('Input_files/PTAA_MAPI_Kloc6_v2.csv');
+%par = pc('Input_files/PTAA_MAPI_Kloc6_v2.csv');
 %par = pc('Input_files/PTAA_MAPI_PCBM_v2.csv');
-%par = pc('Input_files/PTAA_MAPI_ICBA_v2.csv');
+par = pc('Input_files/PTAA_MAPI_ICBA_v2.csv');
 
 eqm_QJV = equilibrate(par);
 CV_sol_ion = doCV(eqm_QJV.ion, 1.15, -0.3, 1.3, -0.3, 1e-3, 1, 321);
