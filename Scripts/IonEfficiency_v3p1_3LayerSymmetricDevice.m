fab413bc47f07cf1afc06de50ad01d1f42186ce4
Lucy <lucy.hart18@imperial.ac.uk>
2022-04-27 15:29:03 +0100

    27/04/22

--- Scripts/IonEfficiency_v3p1_3LayerSymmetricDevice.m
+++ Scripts/IonEfficiency_v3p1_3LayerSymmetricDevice.m
@@ -21,6 +21,7 @@ for i=1:N_y
     end
 end
 
+%%
 error_log = zeros(N_y, NIonConc);
 soleq = cell(N_y, NIonConc);
 solCV_ion = cell(N_y, NIonConc);
@@ -31,9 +32,9 @@ el_results = cell(N_y, NIonConc);
 %% Do (many) JV sweeps
 par=pc('Input_files/3_layer_test_symmetric.csv');
 
-%Reduce voltage limits for undoped TLs (cases with V_BI <= 0.1 only need to
-%go up to 0.4 V)
-for i=1:4
+%Reduce voltage limits for undoped TLs (ion max 1.0V and electron max 1.1V)
+%Use 1.2V as max for doped case.
+for i=1:N_y
     for j=1:NIonConc
         disp(["Ion Concentration = ", num2str(IonConc_values(j)), " cm-3"])
         disp(["Built in potential, Vbi = ", num2str(2*y_values(i)), " V"])
@@ -46,12 +47,12 @@ for i=1:4
 
         soleq{i,j} = equilibrate(par);
         try
-            if params{i,j}(1) <= 0.1
+            if params{i,j}(1) <= -0.1
                 solCV_ion{i, j} = doCV(soleq{i, j}.ion, 1, -0.2, 0.4, -0.2, 1e-4, 1, 263);
                 solCV_el{i, j} = doCV(soleq{i, j}.el, 1, -0.2, 0.4, -0.2, 1e-4, 1, 263);
-            elseif params{i,j}(1) > 0.1
-                solCV_ion{i, j} = doCV(soleq{i, j}.ion, 1, -0.2, 1.2, -0.2, 1e-4, 1, 241);
-                solCV_el{i, j} = doCV(soleq{i, j}.el, 1, -0.2, 1.2, -0.2, 1e-4, 1, 241);
+            elseif params{i,j}(1) > -0.1
+                solCV_ion{i, j} = doCV(soleq{i, j}.ion, 1, -0.2, 1.0, -0.2, 1e-4, 1, 241);
+                solCV_el{i, j} = doCV(soleq{i, j}.el, 1, -0.2, 1.1, -0.2, 1e-4, 1, 241);
             end
             error_log(i,j) = 0;
             ion_results{i,j} = CVstats(solCV_ion{i, j});
@@ -105,33 +106,31 @@ c.Label.String = 'log_{10}(V_{OC,ion} / V_{BI})';
 Vapp = dfana.calcVapp(solCV_ion{5,1});
 Vapp_lowVBI = dfana.calcVapp(solCV_ion{1,1});
 
-voltage_matrix = ones(10,1)*Vapp(18:104);
-voltage_matrix_lowVBI = ones(4,1)*Vapp_lowVBI(44:130);
-voltage_matrix_plot = [voltage_matrix_lowVBI; voltage_matrix];
+voltage_matrix_plot = ones(14,1)*Vapp(1:120);
 
-result_matrix = zeros(N_y, length(voltage_matrix(1,:)));
+result_matrix = zeros(N_y, length(voltage_matrix_plot(1,:)));
 for i = 1:N_y
-    if i <= 4
-        result_matrix(i,:) = dfana.calcJ(solCV_ion{i,4}).tot(44:130,1);
-    elseif i > 4
-        result_matrix(i,:) = dfana.calcJ(solCV_ion{i,4}).tot(18:104,1);
-    end
+    result_matrix(i,:) = dfana.calcJ(solCV_el{i,4}).tot(1:120,1);
 end
 %%
-figure(4)
-[M,h] = contourf(2*y_values(2:end)'*ones(1,87), ...
-    voltage_matrix_plot(2:end,:), ...
-    result_matrix(2:end,:)*1000, ...
-    50);
+figure(6)
+LevelList = linspace(-23,0,50);
+[M,h] = contourf(voltage_matrix_plot(1:end,2:120)', ...
+    (2*y_values(1:end)'*ones(1,119))', ...
+    1000*result_matrix(1:end,2:120)', ...
+    'LevelList', LevelList);
+c = parula;
+%Reverse colourmap
+%c = flipud(c);
+colormap(c)
 hold on
-plot(2*y_values(1:end), 2*y_values(1:end), 'k--')
-patch([0.02,0.3,0.3,0.02],[0.39,0.39,1.1,1.1],'white', 'EdgeColor', 'none')
+plot(2*y_values(1:14), 2*y_values(1:14), 'k--')
 hold off
 h.LineStyle = 'none';
-xlabel('V_{BI} (V)')
-ylabel('Applied Volage (V)')
-ylim([0,1])
-xlim([0.02,1.2])
+ylabel('V_{BI} (V)')
+xlabel('Applied Voltage (V)')
+ylim([0,1.2])
+xlim([-0.19,1.19])
 c = colorbar;
 c.Label.String = 'Current Density (mAcm^{-3})';
 
--- Scripts/weird_cases.m
+++ Scripts/weird_cases.m
@@ -1,5 +1,5 @@
-Ncat = 4;
-y_var = 9;
+Ncat = 1;
+y_var = 14;
 
 J_ion = dfana.calcJ(solCV_ion{y_var, Ncat});
 J_el = dfana.calcJ(solCV_el{y_var, Ncat});
@@ -12,15 +12,15 @@ xlim([-0.15,1.2])
 
 %%
 path = 'C:\Users\ljh3218\OneDrive - Imperial College London\PhD\IonEfficiency\';
-filename = 'tld_symmetric_Vbi_vs_Ncat_DopedTLs_srh.mat';
+filename = 'tld_symmetric_Vbi_vs_Ncat_DopedTLs_0.05eVfromBandEdge_vsr.mat';
 load(append(path,filename));
 
 %%
 
 Ncat = 1;
-y_var = 4;
+y_var = 1;
 
-solCV_ion_single = doCV(soleq{y_var, Ncat}.ion, 1, -0.1, 0.4, -0.1, 1e-3, 1, 241);
-%solCV_el_single = doCV(soleq{y_var, Ncat}.el, 1, -0.2, 0.4, -0.2, 1e-4, 1, 241);
+%solCV_ion_single = doCV(soleq{y_var, Ncat}.ion, 1, -0.2, 1.0, -0.2, 5e-4, 1, 241);
+solCV_el_single = doCV(soleq{y_var, Ncat}.el, 1, -0.2, 1.1, -0.2, 5e-4, 1, 241);
 
   
