12c4454e9c14091a12303e8d793a7cf216c53809
Lucy <lucy.hart18@imperial.ac.uk>
2022-05-11 18:02:38 +0100

    11/05/22

--- Scripts/Weidong_ETL.m
+++ Scripts/Weidong_ETL.m
@@ -2,24 +2,29 @@
 %ETL material used in his FaCs devices. Just without the FaCs..
 
 %% Read in data files 
+num_devices = 3;
 
 par_kloc6 = pc('Input_files/PTAA_MAPI_Kloc6_v2.csv');
 par_pcbm = pc('Input_files/PTAA_MAPI_PCBM_v2.csv');
 par_icba = pc('Input_files/PTAA_MAPI_ICBA_v2.csv');
 par_iph = pc('Input_files/PTAA_MAPI_IPH_v2.csv');
 
-devices = {par_kloc6, par_pcbm, par_icba, par_iph};
+if num_devices == 4
+    devices = {par_kloc6, par_pcbm, par_icba, par_iph};
+elseif num_devices == 3
+    devices = {par_kloc6, par_pcbm, par_icba};
+end
 
 %% Find eqm solutions 
-eqm_solutions_dark = cell(1,4);
-for i = 1:4
+eqm_solutions_dark = cell(1,num_devices);
+for i = 1:num_devices
     eqm_solutions_dark{i} = equilibrate(devices{i});
 end
 
 %% Perform CV scans
-CV_solutions_el = cell(1,4);
-CV_solutions_ion = cell(1,4);
-for j = 1:4
+CV_solutions_el = cell(1,num_devices);
+CV_solutions_ion = cell(1,num_devices);
+for j = 1:num_devices
     CV_solutions_el{j} = doCV(eqm_solutions_dark{j}.el, 1.15, -0.3, 1.3, -0.3, 1e-3, 1, 321);
     CV_solutions_ion{j} = doCV(eqm_solutions_dark{j}.ion, 1.15, -0.3, 1.3, -0.3, 1e-3, 1, 321);
 end
@@ -27,7 +32,7 @@ end
 %% Plot JVs
 figure(1)
 colors_JV = {[0.8500 0.3250 0.0980],[0.4660 0.6740 0.1880],[0 0.4470 0.7410],[0.9290 0.6940 0.1250]};
-for m=1:4
+for m=1:num_devices
     v = dfana.calcVapp(CV_solutions_ion{m});
     v_el = dfana.calcVapp(CV_solutions_el{m});
     j = -dfana.calcJ(CV_solutions_ion{m}).tot(:,1);
@@ -39,7 +44,11 @@ for m=1:4
 end
 plot(v(:), zeros(1,length(v)), 'black', 'LineWidth', 1)
 hold off
-legend({'Kloc-6','', 'PCBM','', 'ICBA','','IPH','',''}, 'Location', 'southwest')
+if num_devices == 4
+    legend({'Kloc-6','', 'PCBM','', 'ICBA','','IPH','',''}, 'Location', 'southwest')
+elseif num_devices == 3
+    legend({'Kloc-6','', 'PCBM','', 'ICBA','',''}, 'Location', 'southwest')
+end
 xlim([0, 1.2])
 ylim([0, 0.027])
 xlabel('Voltage(V)')
@@ -52,11 +61,11 @@ ylabel('Current Density (Acm^{-2})')
 
 num_values = length(CV_solutions_ion{1}.t);
 num_stop = sum(CV_solutions_ion{1}.par.layer_points(1:3));
-J_values = zeros(num_values,7,4);
-J_values_el = zeros(num_values,2,4);
+J_values = zeros(num_values,7,num_devices);
+J_values_el = zeros(num_values,2,num_devices);
 e = -CV_solutions_ion{1}.par.e;
 
-for k=1:4
+for k=1:num_devices
     CVsol = CV_solutions_ion{k};
     loss_currents = dfana.calcr(CVsol,'sub');
     x = CVsol.par.x_sub;
@@ -73,7 +82,7 @@ for k=1:4
     
 end    
 
-for k=1:4
+for k=1:num_devices
     CVsol = CV_solutions_el{k};
     loss_currents = dfana.calcr(CVsol,'sub');
     x = CVsol.par.x_sub;
@@ -86,7 +95,7 @@ end
 %% Plot contributons to the current
 %J_rad not corrected for EL - see EL_Measurements
 figure(3)
-num=3;
+num=2;
 line_colour = {[0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250],[0.4940 0.1840 0.5560]...
                 [0 0.4470 0.7410], [0.3010 0.7450 0.9330], [0.4660 0.6740 0.1880]};
 V = dfana.calcVapp(CV_solutions_ion{1});
@@ -108,7 +117,7 @@ legend({'J_{gen}', 'J_{rad}x100', 'J_{SRH}', 'J_{interface}', 'J_{contact}','J_{
 
 %% Plot PLQY results
 figure(4)
-for i = 1:4
+for i = 1:num_devices
     semilogy(V(1:161), 100*(J_values(1:161,2,i))./J_values(1:161,1,i), 'color', colors_JV{i}) 
     hold on 
     semilogy(V(1:161), 100*(J_values_el(1:161,2,i))./J_values_el(1:161,1,i), '-.', 'color', colors_JV{i})     
@@ -119,7 +128,11 @@ xlim([0, 1.3])
 xlabel('Voltage (V)')
 ylabel('PLQY (%)')
 ylim([1e-5, 0.3])
-legend({'Kloc-6','', 'PCBM','', 'ICBA','','IPH',''}, 'Location', 'northwest')
+if num_devices == 4
+    legend({'Kloc-6','', 'PCBM','', 'ICBA','','IPH',''}, 'Location', 'northwest')
+elseif num_devices == 3
+    legend({'Kloc-6','', 'PCBM','', 'ICBA',''}, 'Location', 'northwest')
+end
 
 
 
