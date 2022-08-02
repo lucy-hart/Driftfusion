46436744fcda006c34d293ca507da877e3dd94eb
Lucy <lucy.hart18@imperial.ac.uk>
2022-08-02 19:16:59 +0100

    02_08_22

--- Scripts/JV_ChangeScanRate.m
+++ Scripts/JV_ChangeScanRate.m
@@ -1,30 +1,34 @@
 % Make plot of JV curves at different scan rates
 
-num = 2;
-scan_rates = [1e-3, 1e-2, 0.05];
-JV_Solutions = cell(1,4);
+num = 3;
+scan_rates = [1e-2, 1e-1, 1];
+JV_Solutions = cell(1,5);
 
 j=1;
 
 for i = scan_rates
-    JV_Solutions{j} = doCV(eqm_solutions_dark{num}.ion, 1.15, -0.3, 1.25, -0.3, i, 1, 321);
+    try
+        JV_Solutions{j} = doCV(eqm_solutions_dark{num}.ion, 1.15, -0.2, 1.2, -0.2, i, 1, 301);
+    catch
+        warning(['JV scan failed for scan rate ' num2str(i) ' Vs-1.'])
+    end
     j = j+1;
 end
 
 %% Plotting 
 
-figure(4)
+figure('Name', 'Scan Rate Dependent JV', 'Position', [100 100 1250 1250])
 for k = 1:3
-plot(v(1:161), -dfana.calcJ(JV_Solutions{k}).tot(1:161,1), 'color', colors_JV{k})
+plot(v(1:151), 1000*dfana.calcJ(JV_Solutions{k}).tot(1:151,1), 'color', colors_JV{k})
 hold on
-plot(v(161:end), -dfana.calcJ(JV_Solutions{k}).tot(161:end,1), 'color', colors_JV{k}, 'LineStyle', '--')
+plot(v(151:end), 1000*dfana.calcJ(JV_Solutions{k}).tot(151:end,1), 'color', colors_JV{k}, 'LineStyle', '--')
 hold on
 end
 
 plot(v(:), zeros(1,length(v)), 'black', 'LineWidth', 1)
 hold off
-legend({'1 mVs^{-1}','','10 mVs^{-1}','', '50 mVs^{-1}',''}, 'Location', 'southwest')
-xlim([0, 1.25])
-ylim([0, 0.025])
+legend({'0.01 Vs^{-1}','','0.1 Vs^{-1}','', '1 Vs^{-1}',''}, 'Location', 'northwest')
+xlim([-0.1, 1.2])
+ylim([-25, 5])
 xlabel('Voltage(V)')
 ylabel('Current Density (Acm^{-2})')
--- Scripts/QuickJVChecker.m
+++ Scripts/QuickJVChecker.m
@@ -1,6 +1,6 @@
 %par = pc('Input_files/PTAA_MAPI_NegOffset.csv');
-par = pc('Input_files/PTAA_MAPI_NoOffset.csv');
-%par = pc('Input_files/PTAA_MAPI_PosOffset.csv');
+%par = pc('Input_files/PTAA_MAPI_NoOffset.csv');
+par = pc('Input_files/PTAA_MAPI_PosOffset.csv');
 
 par.RelTol_vsr = 0.1;
 par = refresh_device(par);
@@ -8,8 +8,10 @@ par = refresh_device(par);
 eqm_QJV = equilibrate(par);
 
 %%
-CV_sol_ion = doCV(eqm_QJV.ion, 1.15, -0.3, 1.3, -0.3, 10e-3, 1, 321);
-CV_sol_el = doCV(eqm_QJV.el, 1.15, -0.3, 1.2, -0.3, 10e-3, 1, 301);
+CV_sol_ion = doCV(eqm_QJV.ion, 1.1, -0.3, 1.2, -0.3, 10e-3, 1, 301);
+CV_sol_el = doCV(eqm_QJV.el, 1.1, -0.3, 1.2, -0.3, 10e-3, 1, 301);
+
+%%
 Plot_Current_Contributions(CV_sol_ion)
 stats_ion = CVstats(CV_sol_ion)
 stats_el = CVstats(CV_sol_el)
