2567834638d9e005087021fb5a8f57ae1f3bec9f
lucy-hart <91886785+lucy-hart@users.noreply.github.com>
2023-06-23 18:32:01 +0100

    23_06_23_PlotDataforLSR

--- Scripts/PlotPCBMDataForLSR.m
+++ Scripts/PlotPCBMDataForLSR.m
@@ -1,18 +1,22 @@
-data = readtable("QFLS_vs_Vap_PCBM_Experimental.xlsx", 'Range', 'A4:B134');
+data = readtable("QFLS_vs_Vap_Experimental.xlsx", 'Range', 'A4:E135');
 
 %%
 figure('Name', 'QFLSPlotExperimental', 'Position', [100 100 1250 2000])
+colours = {[0.4660 0.6740 0.1880],[0 0.4470 0.7410],[0.9290 0.6940 0.1250],[0.8500 0.3250 0.0980],};
 box on
 hold on 
-plot(data{:,1}, data{:,2}, 'color', [0.4660 0.6740 0.1880], 'LineWidth', 3) 
+for i = 1:4
+plot(data{:,1}, data{:,i+1}, 'color', colours{i}, 'LineWidth', 3) 
+end
 
 
 set(gca, 'FontSize', 35)
 xlim([0, 1.3])
-ylim([1, 1.2])
+ylim([1.03, 1.2])
 xlabel('Voltage (V)', 'FontSize', 40)
 ylabel('QFLS (eV)', 'FontSize', 40)
-%legend({'  Mobile Ions', '','  No Mobile Ions',''}, 'Location', 'southeast', 'FontSize', 40)
+legend({'  PCBM', '  ICBA','  IPH','  KLOC-6'}, 'Location', 'northwest', 'FontSize', 40)
+title(legend, 'ETL Material', 'Fontsize', 40)
 ax1 = gca;
 
 %%
@@ -20,6 +24,6 @@ save = 1;
 
 if save == 1 
     exportgraphics(ax1, ...
-    'C:\Users\ljh3218\OneDrive - Imperial College London\PhD\LSR\QFLS_PCBM_Experimental.png', ...
+    'C:\Users\ljh3218\OneDrive - Imperial College London\PhD\LSR\QFLS_Experimental.png', ...
     'Resolution', 300)
 end
