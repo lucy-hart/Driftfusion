f107c912f7bae27b437196ec4046ddc26940c216
lucy-hart <91886785+lucy-hart@users.noreply.github.com>
2023-07-14 10:36:03 +0100

    23_07_13_TrySlowerScanRateDark

--- Scripts/QuickJVChecker.m
+++ Scripts/QuickJVChecker.m
@@ -1,25 +1,27 @@
 parC60 = pc('Input_files/HTL_MAPI_C60_DavideValues.csv');
 parPM6 = pc('Input_files/HTL_MAPI_PM6Y6_DavideValues.csv');
-%parPM7 = pc('Input_files/HTL_MAPI_PM7Y6_DavideValues.csv');
-%parPCE12 = pc('Input_files/HTL_MAPI_PCE12Y6_DavideValues.csv');
+parPM7 = pc('Input_files/HTL_MAPI_PM7Y6_DavideValues.csv');
+parPCE12 = pc('Input_files/HTL_MAPI_PCE12Y6_DavideValues.csv');
 
 
 %%
 run_C60 = 1;
-light = 1;
-noions = 1;
+light = 0;
+noions = 0;
 
 if light == 1
     suns = 1.0;
     Vmin = -0.2;
+    scan_rate = 25e-3;
 elseif light == 0
     suns = 0;
     Vmin = -0.5;
+    scan_rate = 1e-4;
 end
 
 if run_C60 == 1
     eqm_QJV_C60 = equilibrate(parC60);
-    CV_sol_C60 = doCV(eqm_QJV_C60.ion, suns, Vmin, 1.25, Vmin, 25e-3, 1, 291);
+    CV_sol_C60 = doCV(eqm_QJV_C60.ion, suns, Vmin, 1.25, Vmin, scan_rate, 1, 291);
     J_C60 = dfana.calcJ(CV_sol_C60);
     if light == 1
         stats_C60 = CVstats(CV_sol_C60);
@@ -28,27 +30,28 @@ if run_C60 == 1
 end
 
 eqm_QJV_PM6 = equilibrate(parPM6);
-CV_sol_PM6 = doCV(eqm_QJV_PM6.ion, suns, Vmin, 1.25, Vmin, 25e-3, 1, 291);
+CV_sol_PM6 = doCV(eqm_QJV_PM6.ion, suns, Vmin, 1.25, Vmin, scan_rate, 1, 291);
 
-%eqm_QJV_PM7 = equilibrate(parPM7);
-%CV_sol_PM7 = doCV(eqm_QJV_PM7.ion, suns, Vmin, 1.25, Vmin, 25e-3, 1, 291);
+eqm_QJV_PM7 = equilibrate(parPM7);
+CV_sol_PM7 = doCV(eqm_QJV_PM7.ion, suns, Vmin, 1.25, Vmin, scan_rate, 1, 291);
 
-%eqm_QJV_PCE12 = equilibrate(parPCE12);
-%CV_sol_PCE12 = doCV(eqm_QJV_PCE12.ion, suns, Vmin, 1.25, Vmin, 25e-3, 1, 291);
+eqm_QJV_PCE12 = equilibrate(parPCE12);
+CV_sol_PCE12 = doCV(eqm_QJV_PCE12.ion, suns, Vmin, 1.25, Vmin, scan_rate, 1, 291);
 
 if noions == 1
     if run_C60 == 1
-        CV_sol_C60_noions = doCV(eqm_QJV_C60.el, suns, Vmin, 1.25, Vmin, 25e-3, 1, 291);
+        CV_sol_C60_noions = doCV(eqm_QJV_C60.el, suns, Vmin, 1.25, Vmin, scan_rate, 1, 291);
         J_C60_noions = dfana.calcJ(CV_sol_C60_noions);
     end
-    CV_sol_PM6_noions = doCV(eqm_QJV_PM6.el, suns, Vmin, 1.25, Vmin, 25e-3, 1, 291);
+    CV_sol_PM6_noions = doCV(eqm_QJV_PM6.el, suns, Vmin, 1.25, Vmin, scan_rate, 1, 291);
     J_PM6Y6_noions = dfana.calcJ(CV_sol_PM6_noions);
 end
 
+%%
 Vapp = dfana.calcVapp(CV_sol_PM6);
 J_PM6Y6 = dfana.calcJ(CV_sol_PM6);
-%J_PM7Y6 = dfana.calcJ(CV_sol_PM7);
-%J_PCE12Y6 = dfana.calcJ(CV_sol_PCE12);
+J_PM7Y6 = dfana.calcJ(CV_sol_PM7);
+J_PCE12Y6 = dfana.calcJ(CV_sol_PCE12);
 
 %%
 if run_C60 == 1 && light == 1
@@ -81,12 +84,12 @@ if run_C60 == 1 && light == 0
     figure('Name', 'JV Plots', 'Position', [50 50 1000 1000])
 
     box on    
-    plot(Vapp, abs(J_C60.tot(:,1)), 'LineWidth', 2, 'Color', [0 0.4470 0.7410])
+    semilogy(Vapp, abs(J_C60.tot(:,1)), 'LineWidth', 2, 'Color', [0 0.4470 0.7410])
     hold on
     xline(0, 'LineWidth', 2, 'Color', 'black')
-    plot(Vapp, abs(J_PM6Y6.tot(:,1)), 'LineWidth', 2, 'Color', [0.4660 0.6740 0.1880])
-    plot(Vapp, abs(J_PM7Y6.tot(:,1)), 'LineWidth', 2, 'Color', 'red')
-    plot(Vapp, abs(J_PCE12Y6.tot(:,1)), 'LineWidth', 2, 'Color', 'black')
+    semilogy(Vapp, abs(J_PM6Y6.tot(:,1)), 'LineWidth', 2, 'Color', [0.4660 0.6740 0.1880])
+    semilogy(Vapp, abs(J_PM7Y6.tot(:,1)), 'LineWidth', 2, 'Color', 'red')
+    semilogy(Vapp, abs(J_PCE12Y6.tot(:,1)), 'LineWidth', 2, 'Color', 'black')
     hold off
 
     set(gca, 'FontSize', 25)
